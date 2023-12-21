7f4bea8d6926ed4971857e0dc2167a83a44d3a7a
RUSTHT <RUSTHT@gmail.com>
2023-12-22 01:02:11 +0300

    Циклы_Задача №8 апдейт

--- src/cycles/CycleTwo.java
+++ src/cycles/CycleTwo.java
@@ -1,18 +1,26 @@
 package cycles;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class CycleTwo {
-        public static void main(String[] args) {
-          System.out.println("Введите целое положительное число: ");
-            Scanner skinet = new Scanner(System.in);
-              int n = skinet.nextInt();
-              int nechet = 0;
-              for (int i = 1; i <= n; i++) {
-                if (i % 2 != 0) {
-                        nechet = nechet + i;
-                        }
-                }
-                System.out.println("Сумма всех нечетных чисел до " + n + "\nравно " + nechet);
+    public static void main(String[] args) {
+        System.out.println("Введите целое положительное число: ");
+        Scanner skinet = new Scanner(System.in);
+        int n = skinet.nextInt();
+        int nechet = 0;
+        int i = 1;
+        List<String> str = new ArrayList<>();
+        while (i <= n) {
+            str.add(Integer.toString(i));
+            nechet += i;
+            i += 2;
         }
+        if (str.size() <= 20){
+            System.out.println(String.join(" + ", str) + " = " + nechet);
+        } else {
+            System.out.println("Сумма всех нечетных чисел до " + n + "\nравно " + nechet);
+        }
+    }
 }
